53c770793497970d1944d3b51203e815d649275f
Juan <jsebach99@gmail.com>
2025-11-18 19:34:13 -0300

    Agregando script inventory. Bach

--- Assets/Scripts/Gameplay/Items/KeyPickup.cs
+++ Assets/Scripts/Gameplay/Items/KeyPickup.cs
@@ -1,17 +1,25 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class KeyPickup : MonoBehaviour
 {
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-       
-            Destroy(gameObject);
+            // Buscamos el inventario del jugador
+            PlayerInventory inventory = collision.GetComponent<PlayerInventory>();
 
+            if (inventory != null)
+            {
+                inventory.GetKey();   // ✅ Le damos la llave al jugador
+            }
+            else
+            {
+                Debug.LogWarning("El Player no tiene PlayerInventory agregado.");
+            }
 
+            Destroy(gameObject);   // destruimos la llave
         }
     }
 }
+
---
+++ Assets/Scripts/Systems/UI/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string cargarEscena;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerInventory inventory = other.GetComponent<PlayerInventory>();
+
+            if (inventory != null && inventory.hasKey)
+            {
+                Debug.Log($"Cargando escena: {cargarEscena}");
+                SceneManager.LoadScene(cargarEscena);
+            }
+            else
+            {
+                Debug.Log("No podés pasar, te falta la llave 🔒");
+            }
+        }
+    }
+}
